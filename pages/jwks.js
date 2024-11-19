ee45fdbda3ee11bc433d61f88bc65aba8718b7e2
vichanse <vicky.nsenga@gmail.com>
2024-11-19 14:30:13 +0100

    Delete pages/jwks.js

--- pages/jwks.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export default function handler(req, res) {
-  const jwksPath = path.join(process.cwd(), 'public', 'jwks.json')
-  const jwks = fs.readFileSync(jwksPath, 'utf8')
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(jwks);
-}
